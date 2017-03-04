2ddbce92b6f5fc757aadcee5b24d9774044e794e
rayGreycloud <rayGreycloud@gmail.com>
2017-03-04 09:25:15 -0800

    Add paintToCanvas function to grab video and display on canvas

--- scripts.js
+++ scripts.js
@@ -4,16 +4,29 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+
 function getVideo() {
+  // Grab user's video stream
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    // Convert to browser video
     .then(localMediaStream => {
-      console.log(localMediaStream);
       video.src= window.URL.createObjectURL(localMediaStream);
       video.play();
     })
     .catch(err => {
       console.error(`Uh-oh`, err);
     });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(()=> {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
 }
 
 getVideo();
