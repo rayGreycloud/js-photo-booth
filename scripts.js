a20c5ab2fd156689af350caba50f41a6f2891692
rayGreycloud <rayGreycloud@gmail.com>
2017-03-05 06:12:56 -0800

    Add color effect functions

--- scripts.js
+++ scripts.js
@@ -26,6 +26,15 @@ function paintToCanvas() {
 
   return setInterval(()=> {
     ctx.drawImage(video, 0, 0, width, height);
+    // Grab pixels
+    let pixels = ctx. getImageData(0, 0, width, height);
+
+    // Manipulate
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+
+    // Return to canvas
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
 
@@ -40,6 +49,24 @@ function takePhoto() {
   link.setAttribute('download', 'snapshot');
   link.innerHTML = `<img src="${data}" alt="snapshot" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i +=4) {
+    pixels.data[i] += 100;
+    pixels.data[i + 1] -=50;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i +=4) {
+    pixels.data[i - 150] = pixels.data[i];
+    pixels.data[i + 100] = pixels.data[i + 1];
+    pixels.data[i -150] = pixels.data[i + 2];
+  }
+  return pixels;
 }
 
 getVideo();
